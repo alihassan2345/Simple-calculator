0c31279dd0385ac2a49cb565bb7644d86e32132e
= <160609826+alihassan2345@users.noreply.github.com>
2025-03-08 11:36:37 +0500

    simple calculator

---
+++ app.js
@@ -0,0 +1,42 @@
+function foo(e) {
+  let getinp = document.getElementById("inp");
+  let lastChar = getinp.value.slice(-1);
+
+  if (
+    (lastChar === "+" ||
+      lastChar === "-" ||
+      lastChar === "*" ||
+      lastChar === "/" ||
+      lastChar === "%") &&
+    (e === "+" || e === "-" || e === "*" || e === "/" || e === "%")
+  ) {
+    return;
+  }
+
+  getinp.value += e;
+}
+
+function eq() {
+  let getinp = document.getElementById("inp");
+  if (getinp.value == "0") {
+    alert("bhai pehle number select kar ");
+  } else if (
+    getinp.value == "*" ||
+    getinp.value == "/" ||
+    getinp.value == "%"
+  ) {
+    alert("bhai pehle number choose kar ");
+  } else {
+    getinp.value = eval(getinp.value);
+  }
+}
+
+function clr() {
+  let getinp = document.getElementById("inp");
+  getinp.value = getinp.value.slice(0, -1);
+}
+
+function clral() {
+  let getinp = document.getElementById("inp");
+  getinp.value = "";
+}
