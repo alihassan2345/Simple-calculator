b2761ecc3b9c6652d2d1ced332d59d0ba5873c41
= <160609826+alihassan2345@users.noreply.github.com>
2025-03-08 12:44:56 +0500

    Updated calculator logic to prevent multiple operators

--- app.js
+++ app.js
@@ -2,15 +2,9 @@ function foo(e) {
   let getinp = document.getElementById("inp");
   let lastChar = getinp.value.slice(-1);
 
-  if (
-    (lastChar === "+" ||
-      lastChar === "-" ||
-      lastChar === "*" ||
-      lastChar === "/" ||
-      lastChar === "%") &&
-    (e === "+" || e === "-" || e === "*" || e === "/" || e === "%")
-  ) {
-    return;
+ 
+  if ((lastChar === "+" || lastChar === "-" || lastChar === "*" || lastChar === "/" || lastChar === "%") && (e === "+" || e === "-" || e === "*" || e === "/" || e === "%")){
+    return ;
   }
 
   getinp.value += e;
@@ -18,12 +12,12 @@ function foo(e) {
 
 function eq() {
   let getinp = document.getElementById("inp");
-  if (getinp.value == "0") {
+  if (getinp.value === "" || getinp.value === "0") {
     alert("bhai pehle number select kar ");
   } else if (
-    getinp.value == "*" ||
-    getinp.value == "/" ||
-    getinp.value == "%"
+    getinp.value === "*" ||
+    getinp.value === "/" ||
+    getinp.value === "%"
   ) {
     alert("bhai pehle number choose kar ");
   } else {
